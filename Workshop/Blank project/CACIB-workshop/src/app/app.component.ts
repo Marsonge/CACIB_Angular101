8093f7ac4e27da5a85664b8655d83aaf9d5606ca
Tim Docteur CAILLOT-GALLOIS <tim.caillot@gmail.com>
2021-04-05 17:17:57 +0800

    Updated blank project

--- Workshop/Blank project/CACIB-workshop/src/app/app.component.ts
+++ Workshop/Blank project/CACIB-workshop/src/app/app.component.ts
@@ -7,10 +7,17 @@ import { FormControl } from '@angular/forms';
 })
 export class AppComponent implements OnInit{
   title = 'CACIB-workshop';
-  employees = [ { first:'Tim', last:'Caillot'} ]; //Remove this or keep this, it's just to show you how a list works
-  
-  //Everything in here happens when the component inits (like a constructor)
+  employees: Employee[] = [];
+
+
+  // Everything in here happens when the component inits (like a constructor)
   ngOnInit(){
-	    console.log(this.employees.length); //Check the browser console!
+    this.employees.push({ first: 'Tim', last: 'Caillot'}); // Remove this or keep this, it's just to show you how a list works
+    console.log(this.employees.length); // Check the browser console!
   }
+}
+
+export interface Employee {
+  first: string;
+  last: string;
 }
